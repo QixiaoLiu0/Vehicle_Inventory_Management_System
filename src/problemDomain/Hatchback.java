563c619194c9e084ac3abc1818ffdb6ff638699c
Vishavjeet Singh <vishavjeetgill133@gmail.com>
2025-10-22 17:23:52 -0600

    Update the Hatchback class part in theproblrmDomain package

--- src/problemDomain/Hatchback.java
+++ src/problemDomain/Hatchback.java
@@ -5,21 +5,21 @@ public class Hatchback extends Vehicle {
 	
   public Hatchback(String[] arr) {
     super(arr);
-//    this.hatchType = arr[10].charAt(0);
-  }
+    this.hatchType = arr[10].charAt(0);
+}
 	
 	
 	
 	public char getHatchType() {
 		return hatchType;
-		
 	}
 
 
 
 	@Override
 	public String toString() {
-		return "";
+		return super.toString() +
+				"\nHatch Type:\t" + hatchType;
 		
 	}
 
