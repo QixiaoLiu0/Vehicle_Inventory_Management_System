f3e3bd1c6df161f83be97a0705d06a548e5931d1
Tianao0110 <edan0110@gmail.com>
2025-10-22 16:22:37 -0600

    add Display RandomVehicles Action

--- src/action/DisplayRandomVehiclesAction.java
+++ src/action/DisplayRandomVehiclesAction.java
@@ -1,7 +1,7 @@
 package action;
 
 
-import java.util.Scanner;
+import java.util.Scanner; 
 
 import data.VehicleInventory;
 
@@ -10,7 +10,18 @@ public class DisplayRandomVehiclesAction implements Action {
 	@Override
 	public void execute(VehicleInventory inventory, Scanner scanner) {
 		System.out.println("\nEnter number of vehicles to display: ");
-
+		int count = scanner.nextInt();
+	scanner.nextLine();
+	
+		System.out.println("\n\nRandom Vehicles: ");
+		List<Vehicle> randomVehicles = inventory.DisplayRandomVehicles(count);
+		
+		for (int i = 0; i < randomVehicles.size(); i++) {
+			System.out.println(randomVehicles.get(i));
+			if (randomVehicles.size() - i > 1) {
+				System.out.println("----------------------------------");
+			}
+		}
 	}
 
 }
