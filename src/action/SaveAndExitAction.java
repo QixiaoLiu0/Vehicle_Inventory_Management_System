06919df06dbd56b27a1a96d07b299a232909dd95
Vishavjeet Singh <vishavjeetgill133@gmail.com>
2025-10-22 17:16:46 -0600

    updated the saveANDExit part of the project in Action class

--- src/action/SaveAndExitAction.java
+++ src/action/SaveAndExitAction.java
@@ -8,7 +8,13 @@ public class SaveAndExitAction implements Action {
 
 	@Override
 	public void execute(VehicleInventory inventory, Scanner scanner) {
-
+		if(inventory.saveToFile()) {
+			System.out.println("\nSaving Vehicles... Done!");
+			System.out.println("\nGoodbye!");
+			System.exit(0);
+		}else {
+			System.out.println("\nunsaved!");
+		}
 	}
 
 }
