f71ce88713fb674cc44232c4f72190d464d65180
deeparsh5 <deeparsh5@users.noreply.github.com>
2025-10-22 16:51:54 -0600

    completed pickup Action

--- src/action/DisplayPickupByCargoAction.java
+++ src/action/DisplayPickupByCargoAction.java
@@ -1,10 +1,10 @@
 package action;
 
-
+import java.util.List;
 import java.util.Scanner;
 
 import data.VehicleInventory;
-
+import problemDomain.Vehicle;
 
 public class DisplayPickupByCargoAction implements Action {
 
@@ -12,7 +12,14 @@ public class DisplayPickupByCargoAction implements Action {
 	public void execute(VehicleInventory inventory, Scanner scanner) {
 		System.out.println("Enter CargoBeds (SB for Short Beds , EB for Extended Beds, DB for Dump Beds): ");
 		
-
+		String pickupCargoBeds = scanner.nextLine().trim();
+		
+		System.out.println("\nMatching vehicles: ");
+		List<Vehicle> cargoBedsPickup = inventory.DisplayPickupByCargo(pickupCargoBeds);
+		
+		for(Vehicle v : cargoBedsPickup) {
+			System.out.println(v + "\n");
+		}
 	}
 
 }
